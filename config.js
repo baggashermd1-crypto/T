9c2b39d0420bf868b6ec5829d081dd675ebdc359
baggashermd1-crypto <baggashermd1@gmail.com>
2026-04-30 16:48:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -24,12 +24,12 @@ module.exports = {
   BAILEYS: settings.BAILEYS || process.env.BAILEYS || "@whiskeysockets/baileys",
 
   // ===== OWNER & DEVELOPER SETTINGS =====
-  OWNER_NUMBER: settings.OWNER_NUMBER || process.env.OWNER_NUMBER || "923259855436",
+  OWNER_NUMBER: settings.OWNER_NUMBER || process.env.OWNER_NUMBER || "923024927789",
   OWNER_NAME: settings.OWNER_NAME || process.env.OWNER_NAME || "𝘽𝘼𝙂𝙂𝘼-𝙎𝙃𝘼𝙍",
-  DEV: settings.DEV || process.env.DEV || "923259855436",
+  DEV: settings.DEV || process.env.DEV || "923024927789",
   SUDO: settings.SUDO || process.env.SUDO 
     ? (process.env.SUDO || settings.SUDO).split(',').map(s => s.trim()) 
-    : ["923259855436@s.whatsapp.net", "105399168565262@lid", "923259855436@s.whatsapp.net","103131526500560@lid.whatsapp.net",],
+    : ["923024927789@s.whatsapp.net", "105399168565262@lid", "923024927789@s.whatsapp.net","103131526500560@lid.whatsapp.net",],
   BANNED: settings.BANNED || process.env.BANNED 
     ? (process.env.BANNED || settings.BANNED).split(',').map(s => s.trim()) 
     : [],
